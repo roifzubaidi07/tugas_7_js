3ab92783d6a8513af13b93a3cf516a80f9c4f240
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-17 02:59:50 +0700

    Tugas 7 JS
    Tugas ini kami kumpulkan untuk NIOMIC

---
+++ tugas7.js
@@ -0,0 +1,12 @@
+var tinggiBadan = [179, 182, 164, 188, 170, 177, 191, 165, 168, 180];
+
+console.log("Perulangan For :");
+for (let i = 0; i < tinggiBadan.length; i++) {
+  console.log(tinggiBadan[i]);
+}
+
+console.log("");
+console.log("Perulangan For of :");
+for (var x of tinggiBadan) {
+  console.log(x);
+}
